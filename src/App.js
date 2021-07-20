61117210778d6579446fcfd2059ac51a0421efc6
Heggy Castaneda <heggyy@gmail.com>
2021-07-20 12:08:17 -0400

    done setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Welcome to Greeter App!</h1>
         <Greeter name="Sweetie" />
       </div>
     );
